1116f650591edf5ddc733bbf8589bc98cf25862a
Joseph Wilk <joe@josephwilk.net>
2012-05-23 17:16:34 +0100

    Adding coverage of vector_space

---
+++ tests/vector_space_search/test_vector_space.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from vector_space_search.vector_space import VectorSpace
+from nose.tools import *
+from pprint import pprint
+
+class TestVectorSpace(TestCase):
+    def it_should_search_test(self):
+        documents = ["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."]
+
+        vectorSpace = VectorSpace(documents)
+  	
+        eq_(vectorSpace.search(["cat"]), [0.5773502691896258, 0.5, 0.4472135954999579, 0.0])
+
+    def it_should_find_return_similarity_rating_test(self):
+        documents = ["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."]
+
+        vectorSpace = VectorSpace(documents)
+
+        eq_(vectorSpace.related(0), [1.0000000000000002, 0.5773502691896258, 0.2886751345948129, 0.2581988897471611])
--- vector_space_search/vector_space.py
+++ vector_space_search/vector_space.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from Parser import Parser
+from parser import Parser
 from sets import Set
 import sys
 
@@ -11,84 +11,84 @@ except:
 	sys.exit() 
 
 class VectorSpace:
-	""" A algebraic model for representing text documents as vectors of identifiers. 
-	A document is represented as a vector. Each dimension of the vector corresponds to a 
-	separate term. If a term occurs in the document, then the value in the vector is non-zero.
-	"""
+    """ A algebraic model for representing text documents as vectors of identifiers. 
+    A document is represented as a vector. Each dimension of the vector corresponds to a 
+    separate term. If a term occurs in the document, then the value in the vector is non-zero.
+    """
 
-	collection_of_document_term_vectors = []
-	vector_index_to_keyword_mapping = []
+    collection_of_document_term_vectors = []
+    vector_index_to_keyword_mapping = []
 
-	parser=None
+    parser=None
 
-	def __init__(self, documents=[]):
-		self.collection_of_document_term_vectors=[]
-		self.parser = Parser()
-		if(len(documents)>0):
-			self.build(documents)
-
-
-	def build(self,documents):
-		""" Create the vector space for the passed document strings """
-		self.vector_keyword_index = self.get_vector_keyword_index(documents)
-
-		self.collection_of_document_term_vectors = [self.make_vector(document) for document in documents]
+    def __init__(self, documents=[]):
+    	self.collection_of_document_term_vectors=[]
+    	self.parser = Parser()
+    	if(len(documents)>0):
+    		self.build(documents)
 
 
-	def get_vector_keyword_index(self, document_list):
-		""" create the keyword associated to the position of the elements within the document vectors """
-		vocabulary_list = self.parser.tokenise_and_remove_stop_words(document_list)
+    def build(self,documents):
+    	""" Create the vector space for the passed document strings """
+    	self.vector_index_to_keyword_mapping = self.get_vector_keyword_index(documents)
+
+    	self.collection_of_document_term_vectors = [self.make_vector(document) for document in documents]
+
+
+    def get_vector_keyword_index(self, document_list):
+    	""" create the keyword associated to the position of the elements within the document vectors """
+    	vocabulary_list = self.parser.tokenise_and_remove_stop_words(document_list)
         unique_vocabulary_list = self._remove_duplicates(vocabulary_list)
 		
-		vectorIndex={}
-		offset=0
-		#Associate a position with the keywords which maps to the dimension on the vector used to represent this word
-		for word in uniqueVocabularyList:
-			vectorIndex[word]=offset
-			offset+=1
-		return vectorIndex  #(keyword:position)
+    	vector_index={}
+    	offset=0
+    	#Associate a position with the keywords which maps to the dimension on the vector used to represent this word
+    	for word in unique_vocabulary_list:
+    		vector_index[word]=offset
+    		offset+=1
+    	return vector_index  #(keyword:position)
 
 
-	def make_vector(self, word_string):
-		""" @pre: unique(vectorIndex) """
+    def make_vector(self, word_string):
+    	""" @pre: unique(vectorIndex) """
 
-		#Initialise vector with 0's
-		vector = [0] * len(self.vector_keyword_index)
+    	#Initialise vector with 0's
+    	vector = [0] * len(self.vector_index_to_keyword_mapping)
 
-		word_list = self.parser.tokenise_and_remove_stop_words(word_string)
+    	word_list = self.parser.tokenise_and_remove_stop_words(word_string.split(" "))
 
-		for word in wordList:
-			vector[self.vector_keyword_index[word]] += 1; #Use simple Term Count Model
-		return vector
-
-
-	def build_query_vector(self, term_list):
-		""" convert query string into a term vector """
-		query = self.make_vector(" ".join(term_list))
-		return query
+    	for word in word_list:
+            vector[self.vector_index_to_keyword_mapping[word]] += 1; #Use simple Term Count Model
+    	return vector
 
 
-	def related(self,document_id):
-		""" find documents that are related to the document indexed by passed Id within the document Vectors"""
-		ratings = [util.cosine(self.collection_of_document_term_vectors[document_id], document_vector) for document_vector in self.collection_of_document_term_vectors]
-		ratings.sort(reverse=True)
-		return ratings
+    def build_query_vector(self, term_list):
+    	""" convert query string into a term vector """
+    	query = self.make_vector(" ".join(term_list))
+    	return query
 
 
-	def search(self,searchList):
-		""" search for documents that match based on a list of terms """
-		queryVector = self.build_query_vector(searchList)
+    def related(self,document_id):
+    	""" find documents that are related to the document indexed by passed Id within the document Vectors"""
+    	ratings = [self._cosine(self.collection_of_document_term_vectors[document_id], document_vector) for document_vector in self.collection_of_document_term_vectors]
+    	ratings.sort(reverse=True)
+    	return ratings
 
-		ratings = [util.cosine(queryVector, documentVector) for documentVector in self.collection_of_document_term_vectors]
-		ratings.sort(reverse=True)
-		return ratings
 
-    def _remove_duplicates(list):
+    def search(self,searchList):
+    	""" search for documents that match based on a list of terms """
+    	queryVector = self.build_query_vector(searchList)
+
+    	ratings = [self._cosine(queryVector, documentVector) for documentVector in self.collection_of_document_term_vectors]
+    	ratings.sort(reverse=True)
+    	return ratings
+
+    def _remove_duplicates(self, list):
         """ remove duplicates from a list """
         return Set((item for item in list))
     
         
-    def _cosine(vector1, vector2):
+    def _cosine(self, vector1, vector2):
     	""" related documents j and q are in the concept space by comparing the vectors :
     		cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
     	return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
