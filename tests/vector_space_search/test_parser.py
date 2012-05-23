54e4a781607c4a78c2cb43560b942feead0143fc
Joseph Wilk <joe@josephwilk.net>
2012-05-23 16:34:09 +0100

    Nicer nose tools

--- tests/vector_space_search/test_parser.py
+++ tests/vector_space_search/test_parser.py
@@ -1,19 +1,19 @@
 import unittest
 from vector_space_search.parser import Parser
+from nose.tools import *
 
 class TestParser(unittest.TestCase):
-
   def setUp(self):
     self.parser = Parser()
     
   def it_should_remove_the_stop_words_test(self):
     parsed_words = self.parser.tokenise_and_remove_stop_words(["a", "sheep"])
-    self.assertEqual(parsed_words,["sheep"])
+    eq_(parsed_words,["sheep"])
   
   def it_should_stem_words_test(self):
     parsed_words = self.parser.tokenise_and_remove_stop_words(["monkey"])
-    self.assertEqual(parsed_words,["monkei"])
+    eq_(parsed_words,["monkei"])
 
   def it_should_remove_grammar_test(self):
     parsed_words = self.parser.tokenise_and_remove_stop_words(["sheep..."])
-    self.assertEqual(parsed_words, ["sheep"])
+    eq_(parsed_words, ["sheep"])
