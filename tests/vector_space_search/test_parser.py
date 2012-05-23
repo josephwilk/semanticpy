fa44d387bff3093a2b1a2b2244047f5489be955c
Joseph Wilk <joe@josephwilk.net>
2012-05-23 16:29:37 +0100

    Learning nose

--- tests/vector_space_search/test_parser.py
+++ tests/vector_space_search/test_parser.py
@@ -1,8 +1,19 @@
+import unittest
 from vector_space_search.parser import Parser
 
-class TestParser:
-  """"""
+class TestParser(unittest.TestCase):
+
+  def setUp(self):
+    self.parser = Parser()
+    
   def it_should_remove_the_stop_words_test(self):
-    parser = Parser()
-    parser.tokenise_and_remove_stop_words(["words"])
+    parsed_words = self.parser.tokenise_and_remove_stop_words(["a", "sheep"])
+    self.assertEqual(parsed_words,["sheep"])
   
+  def it_should_stem_words_test(self):
+    parsed_words = self.parser.tokenise_and_remove_stop_words(["monkey"])
+    self.assertEqual(parsed_words,["monkei"])
+
+  def it_should_remove_grammar_test(self):
+    parsed_words = self.parser.tokenise_and_remove_stop_words(["sheep..."])
+    self.assertEqual(parsed_words, ["sheep"])
