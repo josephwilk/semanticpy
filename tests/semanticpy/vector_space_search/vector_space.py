193d4e23a2d6459117efaf7cc64546c003cdd54f
Joseph Wilk <joe@josephwilk.net>
2012-06-27 20:22:33 +0100

    Fixing oh so many little annoying things about numpy magically trying to make matrices look like arrays, when really they are not.

---
+++ tests/semanticpy/vector_space_search/vector_space.py
@@ -0,0 +1,103 @@
+from pprint import pprint
+from semanticpy.parser import Parser
+from sets import Set
+import sys
+
+try:
+	from numpy import dot
+	from numpy.linalg import norm
+except:
+	print "Error: Requires numpy from http://www.scipy.org/. Have you installed scipy?"
+	sys.exit() 
+
+class VectorSpace:
+    """ A algebraic model for representing text documents as vectors of identifiers. 
+    A document is represented as a vector. Each dimension of the vector corresponds to a 
+    separate term. If a term occurs in the document, then the value in the vector is non-zero.
+    """
+
+    collection_of_document_term_vectors = []
+    vector_index_to_keyword_mapping = []
+
+    parser=None
+
+    def __init__(self, documents=[]):
+    	self.collection_of_document_term_vectors=[]
+    	self.parser = Parser()
+    	if(len(documents)>0):
+    		self.build(documents)
+
+
+    def build(self,documents):
+    	""" Create the vector space for the passed document strings """
+    	self.vector_index_to_keyword_mapping = self.get_vector_keyword_index(documents)
+
+    	self.collection_of_document_term_vectors = [self.make_vector(document) for document in documents]
+
+
+    def get_vector_keyword_index(self, document_list):
+    	""" create the keyword associated to the position of the elements within the document vectors """
+    	vocabulary_list = self.parser.tokenise_and_remove_stop_words(document_list)
+        unique_vocabulary_list = self._remove_duplicates(vocabulary_list)
+		
+    	vector_index={}
+    	offset=0
+    	#Associate a position with the keywords which maps to the dimension on the vector used to represent this word
+    	for word in unique_vocabulary_list:
+    		vector_index[word]=offset
+    		offset+=1
+    	return vector_index  #(keyword:position)
+
+
+    def make_vector(self, word_string):
+    	""" @pre: unique(vectorIndex) """
+
+    	#Initialise vector with 0's
+    	vector = [0] * len(self.vector_index_to_keyword_mapping)
+
+    	word_list = self.parser.tokenise_and_remove_stop_words(word_string.split(" "))
+
+    	for word in word_list:
+            vector[self.vector_index_to_keyword_mapping[word]] += 1; #Use simple Term Count Model
+    	return vector
+
+
+    def build_query_vector(self, term_list):
+    	""" convert query string into a term vector """
+    	query = self.make_vector(" ".join(term_list))
+    	return query
+
+
+    def related(self,document_id):
+    	""" find documents that are related to the document indexed by passed Id within the document Vectors"""
+    	ratings = [self._cosine(self.collection_of_document_term_vectors[document_id], document_vector) for document_vector in self.collection_of_document_term_vectors]
+    	ratings.sort(reverse=True)
+    	return ratings
+
+
+    def search(self,searchList):
+    	""" search for documents that match based on a list of terms """
+    	queryVector = self.build_query_vector(searchList)
+
+    	ratings = [self._cosine(queryVector, documentVector) for documentVector in self.collection_of_document_term_vectors]
+    	ratings.sort(reverse=True)
+    	return ratings
+
+    def _remove_duplicates(self, list):
+        """ remove duplicates from a list """
+        return Set((item for item in list))
+    
+        
+    def _cosine(self, vector1, vector2):
+    	""" related documents j and q are in the concept space by comparing the vectors :
+    		cosine  = ( V1 * V2 ) / ||V1|| x ||V2|| """
+    	return float(dot(vector1,vector2) / (norm(vector1) * norm(vector2)))
+
+if __name__ == '__main__':
+	#test data
+	documents = ["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."]
+
+	vectorSpace= VectorSpace(documents)
+	pprint(vectorSpace.related(0))
+	pprint(vectorSpace.search(["cat"]))
+
---
+++ tests/tests/unit_tests/lsa_tests/test_tfidf.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from semanticpy.transform.tfidf import Tfidf
+from nose.tools import *
+import numpy
+
+class TestTfidf(TestCase):
+    """ """
+    EPSILON = 4.90815310617e-09
+
+
+    @classmethod
+    def same(self, matrix1, matrix2):
+        difference = matrix1 - matrix2
+        max = numpy.max(difference)
+        return (max <= TestTfidf.EPSILON)
+
+
+    def it_should_do_tfidf_test(self):
+        matrix = [[0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+                  [0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0],
+                  [1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+
+        expected = [[0.,         0.,             0.23104906, 0.,         0.,         0.09589402, 0.,         0.,         0.46209812],
+                    [0.,         0.1732868,      0.,         0.,         0.,         0.07192052, 0.34657359, 0.34657359, 0.        ],
+                    [0.27725887, 0.13862944,     0.,         0.27725887, 0.27725887, 0.05753641, 0.,         0.,         0.        ],
+                    [0.,         0.,             0.69314718, 0.,         0.,         0.,         0.,         0.,         0.        ]]
+
+        expected = numpy.array(expected)
+
+        tfidf = Tfidf(matrix)
+        new_matrix = tfidf.transform()
+
+        eq_(TestTfidf.same(new_matrix, expected), True)
