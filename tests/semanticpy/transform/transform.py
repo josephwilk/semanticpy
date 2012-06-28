64f03c80e36f76f102bfaf70d8fa9b6c38d6f743
Joseph Wilk <joe@josephwilk.net>
2012-06-28 09:38:02 +0100

    Woops, bad import. Whole code base in test folder. nuking

--- tests/semanticpy/transform/transform.py
+++
@@ -1,9 +0,0 @@
-from semanticpy.matrix_formatter import MatrixFormatter
-from scipy import array
-
-class Transform:
-    def __init__(self, matrix):
-        self.matrix = array(matrix)
-
-    def __repr__(self):
-        MatrixFormatter.new(self.matrix).pretty_print
