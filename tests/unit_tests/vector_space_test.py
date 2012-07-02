6c6d28ea5adc57e87d3e167b20c9fe144c6e55dd
Joseph Wilk <joe@josephwilk.net>
2012-07-02 13:18:57 +0100

    Refactor: Isolate the vectorspace unit tests from the transforms

--- tests/unit_tests/vector_space_test.py
+++ tests/unit_tests/vector_space_test.py
@@ -8,11 +8,11 @@ class TestVectorSpace(TestCase):
     documents = ["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."]
 
     def it_should_search_test(self):
-        vector_space = VectorSpace(self.documents)
+        vector_space = VectorSpace(self.documents, transforms = [])
 
-        pprint(vector_space.search(["cat"]))
+        eq_(vector_space.search(["cat"]), [0.5773502691896258, 0.5, 0.4472135954999579, 0.0])
 
     def it_should_find_related(self):
         vector_space = VectorSpace(self.documents)
 
-        pprint(vector_space.related(0))
+        eq_(vector_space.related(0), [])
