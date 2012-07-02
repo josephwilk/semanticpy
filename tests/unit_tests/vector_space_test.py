fbb3cafbd881f0c4c799f567b307b52459cdcea2
Joseph Wilk <joe@josephwilk.net>
2012-07-02 13:54:13 +0100

    Woops, missed naming a method with test in the name.

--- tests/unit_tests/vector_space_test.py
+++ tests/unit_tests/vector_space_test.py
@@ -5,14 +5,14 @@ from nose.tools import *
 
 class TestVectorSpace(TestCase):
 
-    documents = ["The cat in the hat disabled", "A cat is a fine pet ponies.", "Dogs and cats make good pets.","I haven't got a hat."]
+    documents = ["cat", "cat dog","hat"]
 
     def it_should_search_test(self):
         vector_space = VectorSpace(self.documents, transforms = [])
 
-        eq_(vector_space.search(["cat"]), [0.5773502691896258, 0.5, 0.4472135954999579, 0.0])
+        eq_(vector_space.search(["cat"]), [1.0, 0.7071067811865475, 0.0])
 
-    def it_should_find_related(self):
+    def it_should_find_related_test(self):
         vector_space = VectorSpace(self.documents)
 
-        eq_(vector_space.related(0), [])
+        eq_(vector_space.related(0), [1.0000000000000002, 0.9999999999999998, 0.0])
